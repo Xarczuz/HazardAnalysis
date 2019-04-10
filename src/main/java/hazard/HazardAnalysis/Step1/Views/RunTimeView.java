ecf1433fff09b018a908ceac5629007417b5ff3e
xarczuz <chjunchi.chiu@gmail.com>
2019-04-10 13:56:36 +0200

    update

--- src/main/java/hazard/HazardAnalysis/Step1/Views/RunTimeView.java
+++ src/main/java/hazard/HazardAnalysis/Step1/Views/RunTimeView.java
@@ -1,5 +1,16 @@
 package hazard.HazardAnalysis.Step1.Views;
 
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
+
 public class RunTimeView {
+	public static GridPane addGridPane() {
+		GridPane grid = new GridPane();
+		Text text = new Text();
+		text.setText("START VIEW");
+		grid.add(text, 1, 1);
+		//TODO
+		return grid;
+	}
 //TODO
 }
